c29961b15a81e91fc34ea957181090a4231a9654
stefan.jelinovic <stefan.jelinovic@mozzartbet.com>
2024-11-17 22:56:14 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,27 +15,10 @@ export default defineNuxtConfig({
   ssr:true,
 
   nitro: {
-    baseURL: "/",
+    baseURL: "https://smorrior.github.io/nacional-auto",
     prerender: {
       crawlLinks: true,
-      failOnError: false,
-      routes: [
-        '/nacional-auto/o-nama',
-        '/nacional-auto/servis-japanskih-vozila',
-        '/nacional-auto/servis-japanskih-vozila/daihatsu',
-        '/nacional-auto/servis-japanskih-vozila/honda',
-        '/nacional-auto/servis-japanskih-vozila/isuzu',
-        '/nacional-auto/servis-japanskih-vozila/mazda',
-        '/nacional-auto/servis-japanskih-vozila/mitsubishi',
-        '/nacional-auto/servis-japanskih-vozila/nissan',
-        '/nacional-auto/servis-japanskih-vozila/subaru',
-        '/nacional-auto/servis-japanskih-vozila/suzuki',
-        '/nacional-auto/servis-japanskih-vozila/toyota',
-        '/nacional-auto/servis-korejskih-vozila',
-        '/nacional-auto/servis-korejskih-vozila/kia',
-        '/nacional-auto/servis-korejskih-vozila/hyundai',
-        '/nacional-auto/servis-korejskih-vozila/chevrolet',
-      ]
+      failOnError: false
     }
   },
 
